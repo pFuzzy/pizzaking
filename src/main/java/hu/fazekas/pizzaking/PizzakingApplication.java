474e263ee3fb0e0ad483575c55e1539725aa06f1
Fazi <p.fazii888@gmail.com>
2023-07-31 14:10:56 +0200

    Initial commit

---
+++ src/main/java/hu/fazekas/pizzaking/PizzakingApplication.java
@@ -0,0 +1,13 @@
+package hu.fazekas.pizzaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzakingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/fazekas/pizzaking/PizzakingApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.fazekas.pizzaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzakingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
